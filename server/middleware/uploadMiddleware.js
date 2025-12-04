db7c597dba0e415abdf69cf30349e3fbb2282802
ZIN MYO SWE <overmidnight12@gmail.com>
2025-12-05 02:13:17 +0700

    fix multer destination issue on uploadMiddleware

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,7 +1,17 @@
 import multer from 'multer';
+import path from 'path';
 
-const upload = multer({
-  storage: multer.diskStorage(),
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Folder to save files
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + '-' + file.originalname.replace(/\s+/g, '_');
+    cb(null, uniqueName); // File name logic
+  }
 });
 
+const upload = multer({ storage });
+
 export default upload;
+
