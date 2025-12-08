649f8ef3ef3212a5bb3ee48b9c2b96553e93d6aa
ZIN MYO SWE <overmidnight12@gmail.com>
2025-12-08 15:05:06 +0700

    Implement MyBookingData

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,10 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../components/Title'
-import { assets, userBookingsDummyData } from '../assets/assets'
+import { assets } from '../assets/assets'
+import { useAppContext } from '@/context/AppContext'
+import toast from 'react-hot-toast'
 
 const MyBookings = () => {
 
-    const [bookings, setBookings] = useState(userBookingsDummyData)
+    const {axios, getToken, user } = useAppContext();
+    const [bookings, setBookings] = useState([])
+
+    const fetchUserBookings = async () =>{
+        try {
+            const {data} = await axios.get('/api/bookings/user', {
+                headers: {
+                    Authorization: `Bearer ${await getToken()}`
+                }
+            })
+            if(data.success){
+                setBookings(data.bookings);
+            }
+            else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        if(user){
+            fetchUserBookings();
+        }
+    }, [user])
+
 
   return (
     <div className='py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32'>
@@ -23,7 +51,9 @@ const MyBookings = () => {
                 w-full border-b border-gray-300 py-6 first:border-t'>
                     {/* Hotel Details */}
                     <div className='flex flex-col md:flex-row'>
-                        <img src={booking.room.images[0]} alt="hotel-img"
+                        <img
+                        src={booking.room.images && (Array.isArray(booking.room.images[0]) ? booking.room.images[0][2] : booking.room.images[0])}
+                        alt="hotel-img"
                         className='min-md:w-44 rounded shadow object-cover'
                         />
                         <div className='flex flex-col gap-1.5 max-md:mt-3 min-md:ml-4'>
--- server/models/Booking.js
+++ server/models/Booking.js
@@ -4,6 +4,7 @@ const bookingSchema = new mongoose.Schema(
   {
     user: { type: String, ref: "User", required: true },
     room: { type: String, ref: "Room", required: true },
+    hotel: { type: String, ref: "Hotel", required: true },
     checkInDate: { type: Date, required: true },
     checkOutDate: { type: Date, required: true },
     totalPrice: { type: Number, required: true },
