38c4cac7f2851cac96df6b56f1e10fda579117a9
ZIN MYO SWE <overmidnight12@gmail.com>
2025-12-20 21:37:21 +0700

    update Switch for user update and create in webhook

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -19,21 +19,28 @@ const clerkWebhooks = async (req, res) => {
         //Getting data from request body
         const { data, type } = req.body;
 
-        const userData = {
-            _id: data.id,
-            email: data.email_addresses[0].email_address,
-            username: data.first_name+" "+data.last_name,
-            image: data.image_url,
-            
-        }
+        
 
         //Switch Cases for different Events
         switch(type) {
             case 'user.created':{
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name+" "+data.last_name,
+                    image: data.image_url,
+                }
                 await User.create(userData);
                 break;
             }
             case 'user.updated':{
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name+" "+data.last_name,
+                    image: data.image_url,
+                    
+                }
                 await User.findByIdAndUpdate(data.id, userData);
                 break;
             }
